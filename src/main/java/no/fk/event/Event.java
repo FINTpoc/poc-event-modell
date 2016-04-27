823f039707b32d281b494f38ff1e1ba07b522bb9
jarlehansen <jarle@jarlehansen.net>
2016-04-27 20:28:32 +0200

    removed dateTime format, should be handled in the implementation

--- src/main/java/no/fk/event/Event.java
+++ src/main/java/no/fk/event/Event.java
@@ -1,7 +1,5 @@
 package no.fk.event;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import javax.xml.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Date;
@@ -76,7 +74,6 @@ public class Event<T> {
         this.type = type;
     }
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     public Date getTime() {
         return time;
     }
